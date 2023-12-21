a4ec297090a0a977d9a70201935511300bdf8725
kelshardy <14024@coderacademy.edu.au>
2023-12-21 20:51:15 +1100

    fix: user login route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,78 +1,84 @@
-const { User } = require('../models/UserModel');
-const express = require('express');
+const { User } = require("../models/UserModel");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const { comparePassword, generateJwt } = require('../functions/userAuthFunctions');
+const bcrypt = require("bcryptjs");
+const {
+  comparePassword,
+  generateJwt,
+} = require("../functions/userAuthFunctions");
 
 // Create a NEW Account
 router.post("/", async (request, response) => {
-    let newUser = await User.create(request.body)
-    .catch(error => {return error});
+  let newUser = await User.create(request.body).catch((error) => {
+    return error;
+  });
 
-    response.json(newUser);
-})
+  response.json(newUser);
+});
 
 router.post("/login", async (request, response) => {
-
+  try {
     let targetUser = await User.findOne({
-        email: request.body.email
-    }).catch(error => {return error});
+      email: request.body.email,
+    });
+    const isPasswordCorrect = await bcrypt.compare(request.body.password, targetUser.password);
+    // let isPasswordCorrect = comparePassword(
+    //   request.body.password,
+    //   targetUser.password
+    // );
 
-    let isPasswordCorrect = comparePassword(request.body.password, targetUser.password);
-
-    if (!isPasswordCorrect){
-        response.status(403).json({error: "You are not authorized to do this."});
+    if (!isPasswordCorrect) {
+      return response
+        .status(403)
+        .json({ error: "You are not authorized to do this." });
     }
 
     let updatedJwt = generateJwt(targetUser._id);
-    
-    response.json({
-        jwt: updatedJwt
+
+    return response.json({
+      jwt: updatedJwt,
     });
-})
+  } catch (error) {
+    return response.status(401).send("Invalid Email or Password.");
+  }
+});
 
-router.get("/verify", async (request, response) => {
+router.get("/verify", async (request, response) => {});
 
-})
-
-router.get("/regenerate", async (request, response) => {
-
-})
+router.get("/regenerate", async (request, response) => {});
 
 // Get All Users
 router.get("/", async (request, response) => {
-    let users = await User.find({});
-    
-    response.json(users);
-})
+  let users = await User.find({});
+
+  response.json(users);
+});
 
 // Login to an Account
 router.get("/:id", async (request, response) => {
-    let result = await User.findOne({_id: request.params.id});
+  let result = await User.findOne({ _id: request.params.id });
 
-    response.json({result});
-})
+  response.json({ result });
+});
 
 // Edit information on an Account
 router.patch("/", async (request, response) => {
-    try {
-        let result = await User.findByIdAndUpdate(
-            request.params.id, 
-            request.body)
-        response.json(result);
-    } catch (error) {
-        return response.status(500).send(error);
-    }
-})
+  try {
+    let result = await User.findByIdAndUpdate(request.params.id, request.body);
+    response.json(result);
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
 
 // Delete Account
 router.delete("/", async (request, response) => {
-    try {
-        await User.findByIdAndDelete(request.params.id);
-        return response.status(200).send("User Deleted!");
-    } catch (error) {
-        return response.status(500).send(error);
-    }
-})
+  try {
+    await User.findByIdAndDelete(request.params.id);
+    return response.status(200).send("User Deleted!");
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+});
 
 module.exports = router;
