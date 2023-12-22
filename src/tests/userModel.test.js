04260a6a58f34b70ecf0eb0fdf42ca13ec9abb06
kelshardy <14024@coderacademy.edu.au>
2023-12-22 12:28:57 +1100

    feat: test added to user

--- src/tests/userModel.test.js
+++ src/tests/userModel.test.js
@@ -1,6 +1,5 @@
 const { User } = require('../models/UserModel');
 const request = require('supertest');
-const { app } = require('../server');
 const { response } = require('express');
 
 describe("Get existing user", () => {
@@ -8,4 +7,15 @@ describe("Get existing user", () => {
         const getUser = request(User).get("/:id");
         expect(response.body).toEqual(getUser.body);
     });
+})
+
+describe("Create a new user", () => {
+    test("POST route creates a new user", () => {
+        const loginUser = request(User).post("/");
+        const newUser = {
+            email: "user1@email.com",
+            password: "password"
+        }
+        expect(loginUser.body).toEqual(newUser.body);
+    })
 })
